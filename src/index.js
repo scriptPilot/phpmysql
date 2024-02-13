d3bc31d8c4890e1cb841ffd3b2c9f8cd7deaeb08
scriptPilot <dennis@scheffel.de>
2024-02-13 22:54:30 +0100

    consolidate

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 #!/usr/bin/env node
 
-// Log Hello World
-console.log('Hello World 🥳')
+// Import modules
+import path from 'path'
+import fs from 'fs'
+
+// Get installation status
+let isInstalled = false
+try {
+  isInstalled = fs.existsSync(path.resolve('../../../package.json'))
+} catch {}
+console.log(isInstalled ? 'Running as an installed package.' : 'Running in development mode.')
+
+// Create missing index.php file
+if (isInstalled) {
+  const indexPHPFile = path.resolve('../../../public/index.php')
+  if (!fs.existsSync(indexPHPFile)) {
+    fs.writeFileSync(indexPHPFile, '<?php\n\nphpinfo();')
+    console.log('File "public/index.php" created.')
+  }
+}
